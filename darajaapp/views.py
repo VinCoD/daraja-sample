659c4017f0a8662a7c472c8acdad493257b2f780
VinCoD <5895.2020@students.ku.ac.ke>
2023-03-07 11:20:07 +0300

    Intial Commit

---
+++ darajaapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django_daraja.mpesa.core import MpesaClient
+
+def index(request):
+    cl = MpesaClient()
+    phone_number = '0115604647'
+    amount = 1
+    account_reference = 'reference'
+    transaction_desc = 'Description'
+    callback_url = 'https://api.darajambili.com/express-payment'
+    response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
+    return HttpResponse(response)
